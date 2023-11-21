541f9887c26886f313285fd588db37958971a511
Arun Chavan <c.arun13@gmail.com>
2023-11-21 11:57:54 +0100

    Get lists of taxa from the two trees

---
+++ code/01a_get-taxa-from-trees.R
@@ -0,0 +1,30 @@
+# Arun Chavan
+# Started: 2023-11-20
+
+# background ==================================================================
+
+# Here we get lists of taxa from the two mammal trees, dosReis2012 and upham2019
+
+# setup =======================================================================
+library(tidyverse)
+library(here)
+library(ape)
+
+# data ========================================================================
+dr12 <- read.nexus("data/trees/dosReis2012/mammalia_dosReis.tree")
+up19 <- read.nexus("data/trees/upham2019/consensus/DNA-only/MamPhy_fullPosterior_BDvr_DNAonly_4098sp_topoFree_NDexp_MCC_v2_target.tre")
+
+# get taxa ====================================================================
+dr12_tips <- enframe(dr12$tip.label, name = NULL, value = "binomial")
+
+up19_tips <- enframe(up19$tip.label, name = NULL, value = "tiplab")
+
+up19_tips <- up19_tips %>% 
+  mutate(binomial = str_replace(tiplab, ".*([A-Z][a-z]+_[a-z]+).*", "\\1"))
+
+# write lists of taxa to file =================================================
+fs::dir_create("results/taxa")
+write_csv(dr12_tips, "results/taxa/dosReis2012_all-taxa.csv")
+write_csv(up19_tips, "results/taxa/upham2019_all-taxa.csv")
+
+# end =========================================================================
