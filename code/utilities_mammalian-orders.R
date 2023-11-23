986af33964c9033fa2cd01a307e12221b70c9749
Arun Chavan <c.arun13@gmail.com>
2023-11-23 10:22:09 +0100

    clean up and organize trait data

---
+++ code/utilities_mammalian-orders.R
@@ -0,0 +1,53 @@
+# Arun Chavan
+# 2023-11-22
+
+# Mammalian orders ============================================================
+
+Mammalia <- list(
+  Prototheria = list(
+    "Monotremata"
+  ),
+  Metatheria = list(
+    "Didelphimorphia",
+    "Paucituberculata",
+    "Microbiotheria",
+    "Dasyuromorphia",
+    "Peramelemorphia",
+    "Notoryctemorphia",
+    "Diprotodontia"
+  ),
+  Eutheria = list(
+    Xenartha = list(
+      "Pilosa",
+      "Cingulata"
+    ),
+    Afrotheria = list(
+      "Afrosoricida",
+      "Macroscelidea",
+      "Tubulidentata",
+      "Proboscidea",
+      "Sirenia",
+      "Hyracoidea"
+    ),
+    Laurasiatheria = list(
+      "Eulipotyphla",
+      "Soricomorpha",
+      "Erinaceomorpha",
+      "Chiroptera",
+      "Pholidota",
+      "Carnivora",
+      "Perissodactyla",
+      "Artiodactyla",
+      "Cetacea"
+    ),
+    Euarchontoglires = list(
+      "Dermoptera",
+      "Scandentia",
+      "Primates",
+      "Rodentia",
+      "Lagomorpha"
+    )
+  )
+)
+
+# end =========================================================================
