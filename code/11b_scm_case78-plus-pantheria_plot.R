b5c0ed997ebfaddcd09212f8f04681138ef216f3
Arun Chavan <c.arun13@gmail.com>
2026-08-21 22:07:08 -0400

    Working pipeline for SCM. Run case-plus-pantheria.

--- code/11b_scm_case78-plus-pantheria_plot.R
+++ code/11b_scm_case78-plus-pantheria_plot.R
@@ -22,13 +22,7 @@ analysis_name <- "case78-plus-pantheria"
 resdir <- here("results/scm", analysis_name)
 
 # data ========================================================================
-# tipdata
-prec_data <- read_csv(here("data/03_coded/case78-plus-pantheria.csv"))
-prec_data <- prec_data %>%
-  mutate(
-    precocity = fct(precocity, c("altricial", "intermediate", "precocial"))
-  )
-prec_tipdata <- set_names(prec_data$precocity, prec_data$binomial)
+prec_tipdata <- read_rds(here(resdir, "tipdata.rds"))
 
 # consensus results
 tr_consensus <- read_rds(here(resdir, "consensus/tree_pruned.rds"))
@@ -38,19 +32,21 @@ simmap_summary_consensus <- read_rds(here(
 ))
 
 # results from sampled trees
-asr <- tibble(tr_id = 1:100, tr_name = dir(here(resdir, "sample")))
-asr$tr_pruned <- lapply(asr$tr_name, \(x) {
+asr <- read_csv(here(resdir, "treeindices.csv")) |>
+  filter(treename != "consensus")
+
+asr$tr_pruned <- lapply(asr$treename, function(x) {
   read_rds(here(resdir, "sample", x, "tree_pruned.rds"))
 })
-asr$model_weights <- lapply(asr$tr_name, \(x) {
-  read_rds(here(resdir, "sample", x, "model-weights.rds"))
+asr$model_weights <- lapply(asr$treename, function(x) {
+  read_rds(here(resdir, "sample", x, "model-fit.rds"))
 })
-asr$ace <- lapply(asr$tr_name, \(x) {
+asr$ace <- lapply(asr$treename, function(x) {
   read_rds(here(resdir, "sample", x, "ace.rds"))
 })
 
 # taxonomic information =======================================================
-taxa <- prec_data %>%
+taxa <- read_csv(here("data/03_coded/case78-plus-pantheria.csv")) |>
   select(
     rank01,
     rank02,
@@ -70,7 +66,7 @@ taxa <- prec_data %>%
 
 # functions ===================================================================
 get_taxonomic_rank <- function(.taxon) {
-  tax_rank <- names(which(apply(taxa, 2, \(x) any(grepl(.taxon, x)))))
+  tax_rank <- names(which(apply(taxa, 2, function(x) any(grepl(.taxon, x)))))
   if (length(tax_rank) == 0) {
     stop("Taxon not found in data")
   }
@@ -95,9 +91,9 @@ get_classification <- function(.taxon) {
     shortpath <- ranks[seq_len(which(ranks == tax_rank))]
   }
 
-  taxonomy <- taxa %>%
-    select(all_of(shortpath)) %>%
-    filter(.data[[tax_rank]] == .taxon) %>%
+  taxonomy <- taxa |>
+    select(all_of(shortpath)) |>
+    filter(.data[[tax_rank]] == .taxon) |>
     distinct()
 
   stopifnot(nrow(taxonomy) == 1)
@@ -118,8 +114,8 @@ get_pp_df <- function(.taxon) {
   map2_df(asr$tr_pruned, asr$ace, function(x, y) {
     node <- get_node(x, .taxon)
     get_pp_at_node(y, node)
-  }) %>%
-    mutate(tree_id = 1:n()) %>%
+  }) |>
+    mutate(tree_id = 1:n()) |>
     relocate(tree_id)
 }
 
@@ -127,7 +123,7 @@ plot_pp <- function(
   .taxon,
   .title = .taxon
 ) {
-  df <- get_pp_df(.taxon) %>%
+  df <- get_pp_df(.taxon) |>
     pivot_longer(-tree_id, names_to = "precocity", values_to = "pp")
 
   ggplot(df, aes(tree_id, pp, fill = precocity)) +
@@ -140,7 +136,7 @@ plot_pp <- function(
       title = .title,
       caption = paste0(get_classification(.taxon), collapse = " → ")
     ) +
-    theme_bw(base_family = "Source Sans Pro", base_line_size = 0.25) +
+    theme_bw(base_line_size = 0.25) +
     theme(
       axis.text.x = element_text(size = 5),
       axis.text.y = element_text(size = 5),
@@ -163,7 +159,7 @@ plot_and_save_pp_for_all_taxa_in_rank <- function(.rank) {
   x <- unique(taxa[[.rank]][!is.na(taxa[[.rank]])])
   names(x) <- x
   fs::dir_create(here(resdir, "plots/pp", .rank))
-  map(x, possibly(plot_pp)) %>%
+  map(x, possibly(plot_pp)) |>
     iwalk(
       ~ ggsave(
         filename = here(resdir, "plots/pp", .rank, paste0(.y, ".pdf")),
@@ -207,7 +203,6 @@ cairo_pdf(
   here(resdir, "plots", "consensus_asr.pdf"),
   width = 15,
   height = 9,
-  family = "Source Sans Pro",
   pointsize = 14
 )
 par(oma = c(0, 1.5, 0, 1), xpd = NA)
--- code/run_scm.R
+++ code/run_scm.R
@@ -8,8 +8,11 @@
 library(tidyverse)
 library(here)
 library(glue)
+library(phytools)
 library(optparse)
 library(conflicted)
+
+conflicts_prefer(purrr::map)
 
 source(here("code/utilities_scm.R"))
 
@@ -28,47 +31,55 @@ parser <- OptionParser() |>
   ) |>
   add_option(
     "--treelist",
-    help = "Path to a named list object .rds with trees.",
+    help = "Path to a named multiPhylo object .rds.",
     required = TRUE
   ) |>
   add_option(
     "--treeindex",
     help = "Index number of tree to use from treelist",
+    type = "integer",
     required = TRUE
   ) |>
-  add_option("--model", default = "all", help = "ER, SYM, ARD, or all")
+  add_option("--model", default = "all", help = "ER, SYM, ARD, or all") |>
+  add_option(
+    "--nsim",
+    default = 1000,
+    type = "integer",
+    help = "Number of simmaps."
+  )
 
 opt <- parse_args(parser)
+stopifnot(opt$model %in% c("all", "ER", "SYM", "ARD"))
 
-## data =======================================================================
+## data and derived options ===================================================
+seed <- opt$treeindex
+
 treelist <- read_rds(opt$treelist)
 tipdata <- read_rds(opt$tipdata)
 
-
+stopifnot(opt$treeindex <= length(treelist), opt$treeindex >= 1)
 stopifnot(!is.null(names(treelist)))
 stopifnot(!is.null(names(tipdata)))
 
-tr <- treelist[[opt$treeindex]]
+treename <- names(treelist)[opt$treeindex]
+is_consensus <- treename == "consensus"
+
+tr <- treelist[[treename]]
+tipdata <- tipdata[tr$tip.label]
 stopifnot(identical(tr$tip.label, names(tipdata)))
 
-# derived options =============================================================
-seed <- opt$treeindex
-treename <- names(treelist)[treeindex]
-treetype <- ifelse(treename == "consensus", "consensus", "sample")
-
-savesimmap <- ifelse(treetype == "consensus", TRUE, FALSE)
-
 ## setup directories ==========================================================
-if (treetype == "consensus") {
+if (is_consensus) {
   resdir <- here("results/scm", opt$analysis, "consensus")
-}
-if (treetype == "sample") {
+} else {
   resdir <- here("results/scm", opt$analysis, "sample", treename)
 }
+fs::dir_create(resdir)
 
 ###############################################################################
 # run simmap ==================================================================
 ###############################################################################
+set.seed(seed)
 
 if (opt$model == "all") {
   fit <- fit_models(tr, tipdata)
@@ -76,22 +87,23 @@ if (opt$model == "all") {
   fit <- fit_single_model(tr, tipdata, opt$model)
 }
 
-if (treetype == "consensus") {
-  simmap <- run_simmap(fit, 1000)
+if (is_consensus) {
+  message("Running simmap...")
+  simmap <- run_simmap(fit, opt$nsim)
   simmap_summary <- summary(simmap)
   simmap_ace <- simmap_summary$ace
-}
-
-if (treetype == "model") {
-  simmap_ace <- run_simmap_and_get_ace(fit, 1000)
+} else {
+  message("Running simmap...")
+  simmap_ace <- run_simmap_and_get_ace(fit, opt$nsim)
 }
 
 ###############################################################################
 # write output ================================================================
 ###############################################################################
-write_rds(fit, here(resdir, "model-fit.rds"))
-write_rds(simmap_ace, here(resdir, "ace.rds"))
-if (savesimmap) {
+write_rds(fit, here(resdir, "model-fit.rds"), compress = "gz")
+write_rds(simmap_ace, here(resdir, "ace.rds"), compress = "gz")
+write_rds(tr, here(resdir, "tree_pruned.rds"), compress = "gz")
+if (is_consensus) {
   write_rds(simmap_summary, here(resdir, "simmap_summary.rds"), compress = "gz")
 }
 
